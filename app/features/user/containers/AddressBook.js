b1199f6ba6cdf7b66a6c16c0251baa79fe16d6e2
İbrahim Turan <ibrahimturan002@gmail.com>
2020-12-20 22:49:53 +0300

    address list designed

--- app/features/user/containers/AddressBook.js
+++ app/features/user/containers/AddressBook.js
@@ -1,10 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { useImmer } from 'use-immer';
+import { GpFAB } from '../../../components/atoms';
+import { AddressListItem } from '../components';
+import { flex } from '../../../config/styles';
+import styles from './styles';
 
 const AddressBook = () => {
+  const { flex1 } = flex;
+  const { colors } = useTheme();
+
+  const [state] = useImmer({
+    addresses: [
+      {
+        title: 'Home',
+        address: 'Sağlık mah. Güvenlik sok. Altınaykardeşler apt. No:16 D: 10',
+        city: {
+          id: 14,
+          name: 'Bolu',
+        },
+        province: {
+          id: 1,
+          name: 'Merkez',
+        },
+        isDefault: true,
+      },
+      {
+        title: 'Job',
+        address: 'Teknokent D blok Z07',
+        city: {
+          id: 14,
+          name: 'Sakarya',
+        },
+        province: {
+          id: 1,
+          name: 'Serdivan',
+        },
+        isDefault: false,
+      },
+    ],
+  });
+
   return (
-    <View>
-      <Text>Address Book</Text>
+    <View style={flex1}>
+      <ScrollView>
+        {state.addresses.map((address, index) => (
+          <AddressListItem
+            title={address.title}
+            address={address.address}
+            city={address.city.name}
+            province={address.province.name}
+            isDefault={address.isDefault}
+            key={index}
+          />
+        ))}
+      </ScrollView>
+
+      <GpFAB
+        icon="plus"
+        style={styles.fab}
+        backgroundColor={colors.success}
+        color="white"
+      />
     </View>
   );
 };
